d7cf9c2ccb8e24ec6e3b18efd9d3498c4f9baeb3
yesrolya <yesrolya@yandex.ru>
2019-04-02 15:36:25 +1000

    rabotaet

--- RTS_LR2/Program.cs
+++ RTS_LR2/Program.cs
@@ -6,14 +6,10 @@ namespace RTS_LR2
 {
     class Schedule
     {
-        int processes;
-        int tasks;
-        int[,] B;
-        int[,] C;
-        int[] time_vector;
-        int[] L;
-        List<int>[] A;
-        int sum_time;
+        private int processes, tasks, sum_time;
+        private int[,] B, C;
+        private int[] time_vector, L;
+        private List<int>[] A;
 
         public Schedule(int processes, int tasks, int[,] B, int[] time_vector)
         {
@@ -35,14 +31,12 @@ namespace RTS_LR2
             CreateDiagramm();
             DrawDiagram();
         }
-
         void InitSchedule()
         {
             A = new List<int>[processes];
             for (int i = 0; i < processes; i++)
                 A[i] = new List<int>();
         }
-
         //заполнение матрицы достижимости
         private void CreateReachabilityMatrix()
         {
@@ -50,15 +44,12 @@ namespace RTS_LR2
                 for (int j = tasks - 1; j >= 0; j--)
                     if (B[i, j] == 1)
                         C[i, j] = 1;
-
             for (int i = tasks - 1; i >= 0; i--)
                 for (int j = tasks - 1; j >= 0; j--)
                     if (C[i, j] == 1)
-                    {
                         for (int k = 0; k < tasks; k++)
                             if (C[k, i] == 1)
                                 C[k, j] = 1;
-                    }
             Console.WriteLine("Матрица достижимости: ");
             for (int i = 0; i < tasks; i++)
             {
@@ -66,14 +57,12 @@ namespace RTS_LR2
                     Console.Write(C[i, j] + " ");
                 Console.WriteLine();
             }
-             
         }
         //создание списка задач в сооветствии с их приоритетом выполнения
         private void CreatePriorityList()
         {
             int[] pr1 = CountFirstCriterion();
             int[] pr2 = CountSecondCriterion();
-
             int temp1 = pr1.Max() + 10;
             List<int> lst = new List<int>();
             Console.WriteLine("Список значений критериев для вычисления приоритетов: ");
@@ -82,17 +71,13 @@ namespace RTS_LR2
                 Console.WriteLine(i + 1 + ": " + pr1[i] + " " + pr2[i]);
                 lst.Add((pr1[i]*10000) + (10000 - pr2[i]*100) + i);
             }
-            
             lst.Sort();
-
             Console.WriteLine("Список задач по приоритетам: ");
             foreach (var l in lst) Console.Write((l + 1)%100 + " ");
             Console.WriteLine();
             int k = 0;
             foreach (var l in lst)
-            {
                 L[k++] = l%100;
-            }
         }
         //создание списка значений для критерия первого приоритета
         //Наименьшее значение критического по количеству вершин пути от начальной вершины до данной 
@@ -119,7 +104,6 @@ namespace RTS_LR2
                 }
             return maxVal + 1;
         }
-
         //создание списка значений для критерия второго приоритета
         //Наибольшее значение критического по времени пути от данной вершины до конечной 
         private int[] CountSecondCriterion()
@@ -129,29 +113,20 @@ namespace RTS_LR2
                 result[i] = CountPathToEnd(i);
             return result;
         }
-
         //подсчет значения критического по времени пути от данной вершины до конечной
         private int CountPathToEnd(int vertex)
         {
             if (vertex == tasks - 1) return 0;
-
             int maxVal = 0;
             for (int i = 0; i < tasks; i++)
-            {
                 if (B[vertex, i] == 1)
                 {
                     int temp = CountPathToEnd(i);
                     if (temp > maxVal)
-                    {
                         maxVal = temp;
-                    }
                 }
-            }
             return maxVal + time_vector[vertex];
         }
-
-        
-
         private void CreateDiagramm()
         {
             List<Zadacha> zadachi = new List<Zadacha>(); //задачи на выполнении
@@ -163,7 +138,6 @@ namespace RTS_LR2
                 int tempTaskNum = -1;
                 if (CouldBeLoaded(L[j], completed))
                     tempTaskNum = L[j];
-                bool setIt = false;
                 for (int i = 0; i < processes; i++)
                 {
                     if (A[i].Count() < time)
@@ -171,37 +145,41 @@ namespace RTS_LR2
                         SetToSchedule(i, tempTaskNum);
                         if (tempTaskNum != -1)
                         {
-                            setIt = true;
+                            j++;
                             zadachi.Add(new Zadacha(tempTaskNum, time_vector[tempTaskNum]));
                             tempTaskNum = -1;
+                            if (j < tasks && CouldBeLoaded(L[j], completed))
+                                tempTaskNum = L[j];
                         }
                     }
                 }
-
                 for (int k = zadachi.Count() - 1; k >= 0; k--)
                 {
-                    zadachi[k] = zadachi[k] + 1;
+                    zadachi[k] = zadachi[k] - 1;
                     if (zadachi[k].Completed())
                     {
                         completed.Add(zadachi[k].number);
                         zadachi.RemoveAt(k);
                     }
                 }
-                if (setIt) j++;    
                 time++;
-
             }
         }
 
         private void DrawDiagram()
         {
+            Console.WriteLine();
+            int maxTime = 0;
             for (int i = 0; i < processes; i++)
             {
-                string str = i + ": ";
+                if (A[i].Count() > maxTime)
+                    maxTime = A[i].Count();
+                Console.Write(@"{0,8}","CPU" + (i + 1) + ": ");
                 foreach (var a in A[i])
-                    str += (a == -1 ? '-' : a);
-                Console.WriteLine(str);
+                    Console.Write(@"{0,-4}", (a) < 0 ? " " : (a + 1).ToString());
+                Console.WriteLine();
             }
+            Console.WriteLine("Время выполнения: " + maxTime);
         }
         
         private void SetToSchedule(int process_number, int task_number)
@@ -212,43 +190,33 @@ namespace RTS_LR2
             for (int i = 0; i < time; i++)
                 A[process_number].Add(task_number);
         }
-
-
         private bool CouldBeLoaded (int current, List<int> completed)
         {
+            //подсчет количества задач, после которых можно перейти к выполнению данной
             int counter = 0;
             for (int i = 0; i < tasks; i++)
                 if (B[i, current] == 1) counter++;
             if (counter == 0) return true;
+            //поиск выполненных задач, после которых можно перейти к данной
             foreach (var c in completed)
                 if (B[c, current] == 1)
                     return true;
             return false;
         }
-        
     }
-
     public class Zadacha
     {
         public int number;
         public int duration;
-
         public Zadacha (int number, int duration)
         {
             this.number = number;
             this.duration = duration;
         }
-
-        public static Zadacha operator + (Zadacha z1, int num)
-        {
-            return new Zadacha(z1.number, z1.duration + num);
-        }
-
         public static Zadacha operator -(Zadacha z1, int num)
         {
             return new Zadacha(z1.number, z1.duration - num);
         }
-
         public bool Completed ()
         {
             if (duration == 0) return true;
@@ -260,18 +228,14 @@ namespace RTS_LR2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Вариант 8 {0}" +
-                              "3 процессора, " +
-                              "11 задач{0}" +
-                              "Критерии:{0}" +
+            Console.WriteLine("Вариант 8 {0}" + "3 процессора, " + "11 задач{0}" + "Критерии:{0}" +
                               "Наименьшее значение критического по количеству вершин пути от начальной вершины до данной/{0}" +
                               "Наибольшее значение критического по времени  пути от данной вершины до конечной{0}",
                               Environment.NewLine);
             //наименования переменных соответствуют методичке
             int m = 3; //количество процессоров
             int n = 11; //количество задач
-                        //вектор времен
-            int[] T = { 3, 2, 2, 3, 4, 2, 3, 4, 2, 3, 4 };
+            int[] T = { 3, 2, 2, 3, 4, 2, 3, 4, 2, 3, 4 }; //вектор времен
             //матрица смежности
             int[,] B = {{0,1,1,1,0,0,0,0,0,0,0},
                         {0,0,0,0,0,1,0,0,0,0,0},
@@ -284,7 +248,6 @@ namespace RTS_LR2
                         {0,0,0,0,0,0,0,0,0,0,1},
                         {0,0,0,0,0,0,0,0,0,0,1},
                         {0,0,0,0,0,0,0,0,0,0,0}};
-
             Schedule sc = new Schedule(m, n, B, T);
             Console.ReadKey();
         }
